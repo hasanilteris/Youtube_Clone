8be66b230facd27c198b7a54bed174a61fd71e6e
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2023-10-04 22:06:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('#menu');
 const sidebar = document.querySelector('.sidebar');
 
+
+
 menu.addEventListener('click',function(){
     sidebar.classList.toggle('show-sidebar');
 })
